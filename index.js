20ba7859d0a9eff1ad82891c4d8276782b9ec6b7
Teddy <teddywambua5456@gmail.com>
2023-03-18 01:32:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+console.log(multiply); 
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+console.log(random); 
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+
+console.log(mod); 
+
+let numbers = [5, 9, 20, 6, 2];
+let max = Math.max(...numbers);
+
+console.log(max); 
